c396b90f3c70da18489a93f3142f5bacc9246a88
Samuel <samuel.ofc.lopes@gmail.com>
2026-04-18 10:55:57 +0100

    1/2 Da atualização para suporte multiplataforma

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import subprocess # Para executar comandos no terminal.
 import os # Apenas para limpar o terminal, já que subprocess não limpa corretamente em algumas IDEs.
- 
+import platform
 
-# Dicionário de apps para futuras atualizações
-apps = {
+if platform.system() == "Windows":
+    Sys_installer = ["winget"]
+    apps = {
     "VSCode": "Microsoft.VisualStudioCode", 
     "IntelliJ": "JetBrains.IntelliJIDEA.Community", 
     "WebStorm": "JetBrains.WebStorm ", 
@@ -27,6 +28,12 @@ apps = {
     "Figma": "Figma.Figma", 
     "Canva": "Canva.Canva"
 }
+elif platform.system() == "Linux":
+    Sys_installer = ["sudo", "apt"]
+    
+elif platform.system() == "Darwin":
+    Sys_installer = ["brew"]
+
 
 # Lista das apps em ordem alfabética
 apps_abc = sorted(apps)
@@ -38,9 +45,9 @@ apps_upper = {app.upper(): apps[app] for app in sorted(apps)}
 
 
 # Execussão do código no terminal
-def escolha(type, esc):
+def escolha(esc):
     try:
-        resultado = subprocess.run(["winget", type, apps_upper[esc]], capture_output=True, text=True) # Captura o output e transforma de bytes para texto
+        resultado = subprocess.run(esc, capture_output=True, text=True) # Captura o output e transforma de bytes para texto
         print(resultado.stdout.splitlines()[-1]) # Printa o resultado
     except KeyError:
         print("No app found.")
@@ -49,19 +56,30 @@ def escolha(type, esc):
 while True:
     print("========================================================================================================================================")
     print("Welcome to app installer\n" \
-          "Choose from the apps listed below the ones you would like to install / update / uninstall.\n")
+          "Choose from the apps listed below the ones you would like to install / upgrade / uninstall.\n")
     print("========================================================================================================================================")
+    
+    # PRINTA A TABELA
     for i in range(0, len(apps_abc) - 3, 1):
         if i & 3 == 0:
             print("\n")
         print(" | " + apps_abc[i] + (" " * (len(max(apps_abc, key=len)) - len(apps_abc[i]))), end="")
         
     print("\n\n\n========================================================================================================================================")
-    action = input("What you want to do? [Install / Update / Uninstall]").strip().upper()
-    while action not in ["INSTALL", "UPDATE", "UNINSTALL"]:
-        action = input("Invalid option, try again [Install / Update / Uninstall]").strip().upper()
-    escolha(action, input("\nEnter the name: ").strip().upper())
+    
+    # EXECUTA O QUE É PEDIDO
+    action = input("What you want to do? [Install / Upgrade / Uninstall]").strip().upper()
+    while action not in ["INSTALL", "UPGRADE", "UNINSTALL"]:
+        action = input("Invalid option, try again [Install / Upgrade / Uninstall]").strip().upper()
+    choosed_app = ""
+    while choosed_app not in apps_upper:
+        choosed_app = input("\nEnter the name: ").strip().upper()
+    command = Sys_installer + [action, apps_upper[choosed_app]]
+    escolha(command)
+
     print("========================================================================================================================================")
+    
+    # CONTINUA OU PARA O CODIGO
     mais = input("Do you want to do something else? [Y/N]: ").upper()
 
     while mais not in ["YES", "Y", "SIM", "S", "NO", "N", "NÃO", "NAO"]:
